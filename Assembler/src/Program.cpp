eeccf3415cfb538ec3762b72cae3f75f2ddc178b
Oyale <salamivictor15@gmail.com>
2024-07-30 13:06:48 +0100

    Sets up the default project folders

---
+++ Assembler/src/Program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Assembler.h"
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Assembler Entry Point" << std::endl;
+	std::cin.get();
+	return 0;
+}
---
+++ Disassembler/src/Program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Disassembler.h"
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Dissasembler Entry Point" << std::endl;
+	std::cin.get();
+	return 0;
+}
